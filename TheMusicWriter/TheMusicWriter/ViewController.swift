f8a86770b08e708e7ecf5f96cc7944868c4c964b
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-07-01 15:29:09 -0600

    added CocoaPod

--- TheMusicWriter/TheMusicWriter/ViewController.swift
+++ TheMusicWriter/TheMusicWriter/ViewController.swift
@@ -119,6 +119,44 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     
     
+    // -------------------------------------------------------------------------
+    
+    @IBOutlet weak var playOutlet: UIButton!
+    @IBAction func playMelodyClicked(_ sender: UIButton) {
+    }
+    
+    
+//    var sequence : MusicSequence? = nil
+//    var musicSequence = NewMusicSequence(&sequence)
+//
+//    var track : MusicTrack? = nil
+//    var musicTrack = MusicSequenceNewTrack(sequence!, &track)
+//
+//    // Adding notes
+//
+//    var time = MusicTimeStamp(1.0)
+//    for index:UInt8 in 60...72 { // C4 to C5
+//        var note = MIDINoteMessage(channel: 0,
+//                                   note: index,
+//                                   velocity: 64,
+//                                   releaseVelocity: 0,
+//                                   duration: 1.0 )
+//        musicTrack = MusicTrackNewMIDINoteEvent(track!, time, &note)
+//        time += 1
+//    }
+//
+//    // Creating a player
+//
+//    var musicPlayer : MusicPlayer? = nil
+//    var player = NewMusicPlayer(&musicPlayer)
+//
+//    player = MusicPlayerSetSequence(musicPlayer!, sequence)
+//    player = MusicPlayerStart(musicPlayer!)
+//
+//    PlaygroundPage.current.needsIndefiniteExecution = true
+    // -----------------------------------------------------------------------------
+    
+    
     
     // [note Octave # : frequency]
     // middle C is C4
